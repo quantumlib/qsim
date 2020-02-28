7809fe4bb2b23116b962a3e8a1808786520ef873
Sergei Isakov <iserge@google.com>
2020-02-28 18:37:41 +0100

    Delete -lgomp compiler flag.

--- tests/make.sh
+++ tests/make.sh
@@ -25,8 +25,8 @@ g++ -O3 -I$path_to_include -L$path_to_lib -o circuit_reader_test circuit_reader_
 g++ -O3 -I$path_to_include -L$path_to_lib -o fuser_basic_test fuser_basic_test.cc -lpthread -lgtest
 g++ -O3 -I$path_to_include -L$path_to_lib -o gates_def_test gates_def_test.cc -lpthread -lgtest
 g++ -O3 -I$path_to_include -L$path_to_lib -o matrix_test matrix_test.cc -lpthread -lgtest
-g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -lgomp -o hybrid_test hybrid_test.cc -lpthread -lgtest
-g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -lgomp -o run_qsim_test run_qsim_test.cc -lpthread -lgtest
-g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -lgomp -o run_qsimh_test run_qsimh_test.cc -lpthread -lgtest
-g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -lgomp -o simulator_avx_test simulator_avx_test.cc -lpthread -lgtest
-g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -lgomp -o simulator_basic_test simulator_basic_test.cc -lpthread -lgtest
+g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -o hybrid_test hybrid_test.cc -lpthread -lgtest
+g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -o run_qsim_test run_qsim_test.cc -lpthread -lgtest
+g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -o run_qsimh_test run_qsimh_test.cc -lpthread -lgtest
+g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -o simulator_avx_test simulator_avx_test.cc -lpthread -lgtest
+g++ -O3 -I$path_to_include -L$path_to_lib -mavx2 -mfma -fopenmp -o simulator_basic_test simulator_basic_test.cc -lpthread -lgtest
