4f530a2d96152d52bdef30bb2d09669aacedf58b
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2020-03-27 10:02:56 -0700

    Convert to Sergei's implementation.

--- lib/state_sampler.h
+++ lib/state_sampler.h
@@ -12,47 +12,41 @@ class StateSampler {
  public:
   using State = typename StateSpace::State;
 
-  // Function to draw m samples from a StateSpace Object in
-  // O(2 ** num_qubits + m * log(m)) time.
-  // Samples are stored as bit encoded integers.
-  //
-  // This method takes a seed for the RNG. To choose a random seed,
-  // Use the other version of this method.
-  void SampleState(const StateSpace& statespace, const State& state,
-                   const int m, unsigned seed,
+  void SampleState(const StateSpace& state_space, const State& state,
+                   const unsigned num_samples, unsigned seed,
                    std::vector<uint64_t>* samples) {
-    if (m == 0) {
+    if (num_samples <= 0) {
       return;
     }
-    std::mt19937 mt(seed);
-    std::uniform_real_distribution<float> dist(0.0, 1.0);
+    samples->reserve(num_samples);
 
-    double cdf_so_far = 0.0;
-    std::vector<float> random_vals(m, 0.0);
-    samples->reserve(m);
-    for (int i = 0; i < m; i++) {
-      random_vals[i] = dist(mt);
+    double norm = 0;
+    for (uint64_t k = 0; k < state_space.Size(state); ++k) {
+      norm += std::norm(state_space.GetAmpl(state, k));
     }
-    std::sort(random_vals.begin(), random_vals.end());
 
-    int j = 0;
-    for (uint64_t i = 0; i < statespace.Size(state); i++) {
-      const std::complex<float> f_amp = statespace.GetAmpl(state, i);
-      const std::complex<double> d_amp = std::complex<double>(
-        static_cast<double>(f_amp.real()),
-        static_cast<double>(f_amp.imag()));
-      cdf_so_far += std::norm(d_amp);
-      while (random_vals[j] < cdf_so_far && j < m) {
-        samples->push_back(i);
-        j++;
+    std::mt19937_64 rgen(seed);
+    double rgen_norm = norm / std::mt19937_64::max();
+
+    std::vector<double> rs;
+    rs.reserve(num_samples);
+
+    for (unsigned i = 0; i < num_samples; ++i) {
+      rs.emplace_back(rgen() * rgen_norm);
+    }
+
+    std::sort(rs.begin(), rs.end());
+
+    unsigned m = 0;
+    double csum = 0;
+
+    for (uint64_t k = 0; k < state_space.Size(state); ++k) {
+      csum += std::norm(state_space.GetAmpl(state, k));
+
+      while (m < num_samples && rs[m] <= csum) {
+        samples->emplace_back(k);
+        ++m;
       }
-    }
-
-    // Safety measure in case of state norm underflow.
-    // Likely to not have huge impact.
-    while (j < m) {
-      samples->push_back(samples->at(samples->size() - 1));
-      j++;
     }
   }
 };
