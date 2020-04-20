bd56ce6a92df3755e99717c13387a3fd215324c7
Sergei Isakov <iserge@google.com>
2020-04-20 19:50:28 +0200

    Add SSE simulator.

--- tests/statespace_sse_test.cc
+++ tests/statespace_sse_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "statespace_test.h"
+#include "statespace_testfixture.h"
 
 #include "gtest/gtest.h"
 
