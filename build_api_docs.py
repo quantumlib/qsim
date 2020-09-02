60e1748fb236509c7c4a0aef00285f089fabc011
Michael Broughton <mbbrough@google.com>
2020-09-02 13:01:20 -0700

    format.

--- build_api_docs.py
+++ build_api_docs.py
@@ -13,11 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """Tool to generate external api_docs for qsim shameless copy from TFQ."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 
 from absl import app
