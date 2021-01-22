e38be7dd6068e09296cba9de51f8640e71708bab
Michael Broughton <mbbrough@google.com>
2021-01-21 17:36:21 -0800

    Added two qubit controlled gates to unitary.

--- lib/unitary_calculator_basic.h
+++ lib/unitary_calculator_basic.h
@@ -18,6 +18,7 @@
 #include <cstdint>
 #include <vector>
 
+#include "bits.h"
 #include "unitaryspace_basic.h"
 
 namespace qsim {
@@ -69,8 +70,9 @@ class UnitaryCalculatorBasic final {
                            const fp_type* matrix, Unitary& state) const {
     if (qs.size() == 1) {
       ApplyControlledGate1(qs[0], cqs, cmask, matrix, state);
+    } else if (qs.size() == 2) {
+      ApplyControlledGate2(qs[0], qs[1], cqs, cmask, matrix, state);
     }
-    // Implement 2 qubit version.
   }
 
  private:
@@ -157,6 +159,101 @@ class UnitaryCalculatorBasic final {
     uint64_t size = uint64_t{1} << n;
 
     unsigned kk = 1;
+    unsigned nn = num_qubits_ > kk ? num_qubits_ - kk : 0;
+    uint64_t size2 = uint64_t{1} << nn;
+
+    for_.Run(size * size2, f, matrix, ms, xss, num_qubits_, size2, cmaskh,
+             emaskh, rstate);
+  }
+
+  void ApplyControlledGate2(unsigned q0, unsigned q1,
+                            const std::vector<unsigned>& cqs, uint64_t cmask,
+                            const fp_type* matrix, State& state) const {
+    uint64_t xs[2];
+    uint64_t ms[3];
+
+    xs[0] = uint64_t{1} << (q0 + 1);
+    ms[0] = (uint64_t{1} << q0) - 1;
+
+    xs[1] = uint64_t{1} << (q1 + 1);
+    ms[1] = ((uint64_t{1} << q1) - 1) ^ (xs[0] - 1);
+
+    ms[2] = ((uint64_t{1} << num_qubits_) - 1) ^ (xs[1] - 1);
+
+    uint64_t xss[4];
+    for (unsigned i = 0; i < 4; ++i) {
+      uint64_t a = 0;
+      for (uint64_t k = 0; k < 2; ++k) {
+        if (((i >> k) & 1) == 1) {
+          a += xs[k];
+        }
+      }
+      xss[i] = a;
+    }
+
+    uint64_t emaskh = 0;
+
+    for (auto q : cqs) {
+      emaskh |= uint64_t{1} << q;
+    }
+
+    uint64_t cmaskh = bits::ExpandBits(cmask, num_qubits_, emaskh);
+
+    emaskh |= uint64_t{1} << q0;
+    emaskh |= uint64_t{1} << q1;
+
+    emaskh = ~emaskh;
+
+    auto f = [](unsigned n, unsigned m, uint64_t ii, const fp_type* v,
+                const uint64_t* ms, const uint64_t* xss, unsigned n_qb,
+                unsigned sqrt_size, uint64_t cmaskh, uint64_t emaskh,
+                fp_type* rstate) {
+      fp_type rn, in;
+      fp_type rs[16], is[16];
+
+      auto row_size = uint64_t{1} << n_qb;
+
+      uint64_t i = ii % sqrt_size;
+      uint64_t j = ii / sqrt_size;
+
+      uint64_t col_loc = (1 * i & ms[0]) | (2 * i & ms[1]) | (4 * i & ms[2]);
+      uint64_t row_loc = bits::ExpandBits(j, n_qb, emaskh) | cmaskh;
+
+      auto p0 = rstate + row_size * 2 * row_loc + 2 * col_loc;
+
+      for (unsigned l = 0; l < 4; ++l) {
+        for (unsigned k = 0; k < 4; ++k) {
+          rs[4 * l + k] = *(p0 + xss[l] * row_size + xss[k]);
+          is[4 * l + k] = *(p0 + xss[l] * row_size + xss[k] + 1);
+        }
+      }
+
+      for (unsigned l = 0; l < 4; l++) {
+        uint64_t j = 0;
+        for (unsigned k = 0; k < 4; ++k) {
+          rn = rs[l] * v[j] - is[l] * v[j + 1];
+          in = rs[l] * v[j + 1] + is[l] * v[j];
+          j += 2;
+
+          for (unsigned p = 1; p < 4; ++p) {
+            rn += rs[4 * p + l] * v[j] - is[4 * p + l] * v[j + 1];
+            in += rs[4 * p + l] * v[j + 1] + is[4 * p + l] * v[j];
+
+            j += 2;
+          }
+          *(p0 + xss[k] * row_size + xss[l]) = rn;
+          *(p0 + xss[k] * row_size + xss[l] + 1) = in;
+        }
+      }
+    };
+
+    fp_type* rstate = state.get();
+
+    unsigned k = 2 + cqs.size();
+    unsigned n = num_qubits_ > k ? num_qubits_ - k : 0;
+    uint64_t size = uint64_t{1} << n;
+
+    unsigned kk = 2;
     unsigned nn = num_qubits_ > kk ? num_qubits_ - kk : 0;
     uint64_t size2 = uint64_t{1} << nn;
 
@@ -259,9 +356,9 @@ class UnitaryCalculatorBasic final {
       xss[i] = a;
     }
 
-    auto f = [q0, q1](unsigned n, unsigned m, uint64_t ii, const fp_type* v,
-                      const uint64_t* ms, const uint64_t* xss, unsigned n_qb,
-                      unsigned sqrt_size, fp_type* rstate) {
+    auto f = [](unsigned n, unsigned m, uint64_t ii, const fp_type* v,
+                const uint64_t* ms, const uint64_t* xss, unsigned n_qb,
+                unsigned sqrt_size, fp_type* rstate) {
       fp_type rn, in;
       fp_type rs[16], is[16];
 
