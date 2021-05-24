27ddd42c19db7bb852a33510cc33c8fc7ca6d0bc
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-05-24 07:10:34 -0700

    Update version to 0.9.4 and Python version to 3.6

--- setup.py
+++ setup.py
@@ -75,14 +75,14 @@ description = "Schrödinger and Schrödinger-Feynman simulators for quantum circ
 # README file as long_description.
 long_description = open("README.md", encoding="utf-8").read()
 
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 
 setup(
     name="qsimcirq",
     version=__version__,
     author="Vamsi Krishna Devabathini",
     author_email="devabathini92@gmail.com",
-    python_requires=">=3.3.0",
+    python_requires=">=3.6.0",
     install_requires=requirements,
     license="Apache 2",
     description=description,
