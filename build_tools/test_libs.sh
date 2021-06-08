1bca2888a330f903c05758e5c5ea3b51f55c30ae
Laurynas Tamulevicius <laurynast@google.com>
2021-06-08 10:52:09 +0300

    Run only basic tests

--- build_tools/test_libs.sh
+++ build_tools/test_libs.sh
@@ -6,11 +6,18 @@ which bazel
 bazel version
 
 # Attempt to build all components in SSE and basic mode.
-# The Kokoro MacOS VMs are not configured for AVX2 or OpenMP, so these modes
+# The Github Actions MacOS VMs are not configured for AVX2 or OpenMP, so these modes
 # are excluded from the build and test process.
 bazel build --config=sse apps:all
-bazel build --config=avx apps:all
 bazel build apps:all
 
 # Run all basic tests.
-bazel test --test_output=errors tests:all
+set +e  # Ignore errors until artifacts are collected.
+EXIT_CODE=0
+for TARGET in bitstring_test circuit_qsim_parser_test fuser_basic_test \
+              gates_qsim_test hybrid_test matrix_test run_qsim_test \
+              run_qsimh_test simulator_basic_test statespace_basic_test; do \
+  if ! bazel test --test_output=errors tests:${TARGET}; then
+    EXIT_CODE=1
+  fi
+done
