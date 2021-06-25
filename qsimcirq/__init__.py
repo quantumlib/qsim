bca1ef9c4973ec6687de2ab3f2e75bb7db44be4e
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-06-25 09:58:57 -0700

    Expose qsimcirq.__version__

--- qsimcirq/__init__.py
+++ qsimcirq/__init__.py
@@ -1,3 +1,7 @@
 from .qsim_circuit import add_op_to_opstring, add_op_to_circuit, QSimCircuit
 from .qsim_simulator import QSimSimulatorState, QSimSimulatorTrialResult, QSimSimulator
 from .qsimh_simulator import QSimhSimulator
+
+from qsimcirq._version import (
+    __version__,
+)
