95804984180714c4951bc429e3d9371d5d793950
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-09-21 08:47:32 -0700

    Autoformat python

--- docs/tutorials/multinode/terraform/htcondor/noise.py
+++ docs/tutorials/multinode/terraform/htcondor/noise.py
@@ -4,11 +4,7 @@ import cirq, qsimcirq
 
 # Create a Bell state, |00) + |11)
 q0, q1 = cirq.LineQubit.range(2)
-circuit = cirq.Circuit(
-    cirq.H(q0),
-    cirq.CNOT(q0, q1),
-    cirq.measure(q0, q1, key='m')
-)
+circuit = cirq.Circuit(cirq.H(q0), cirq.CNOT(q0, q1), cirq.measure(q0, q1, key="m"))
 
 # Constructs a noise model that adds depolarizing noise after each gate.
 noise = cirq.NoiseModel.from_noise_model_like(cirq.depolarize(p=0.05))
@@ -21,4 +17,4 @@ result = sim.run(noisy_circuit, repetitions=1000)
 # Outputs a histogram dict of result:count pairs.
 # Expected result is a bunch of 0s and 3s, with fewer 1s and 2s.
 # (For comparison, the noiseless circuit will only have 0s and 3s)
-print(result.histogram(key='m'))
+print(result.histogram(key="m"))
