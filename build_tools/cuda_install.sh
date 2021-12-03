43db2caeac449dd0a858a081a63e7e43b6d6401f
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-12-03 09:50:30 -0800

    script-based CUDA install

---
+++ build_tools/cuda_install.sh
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+sudo python3 install_gpu_driver.py --force
