1142a18195ea18d3228753cb151d3719bddddccc
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-12-03 11:47:24 -0800

    ooc install, ic no pre-install

--- build_tools/cuda_install.sh
+++ build_tools/cuda_install.sh
@@ -1,10 +1,4 @@
 curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
 
-# pre-install dependencies to prevent reboot
-kernel_version=$(uname -r)
-yum clean all
-yum install -y kernel
-yum install -y kernel-devel-${kernel_version} kernel-headers-${kernel_version} pciutils
-
 # run the installer script
 python3 install_gpu_driver.py --force
