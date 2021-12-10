3a6b86f47d0987cd4db6150ef4fc50cf7e342aaa
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-12-10 11:08:14 -0800

    need more data

--- build_tools/cuda_install.sh
+++ build_tools/cuda_install.sh
@@ -25,4 +25,7 @@ echo "export LD_LIBRARY_PATH=/usr/local/cuda-11/lib64:$LD_LIBRARY_PATH" >> $CUDA
 export PATH=/usr/local/cuda-11/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11/lib64:$LD_LIBRARY_PATH
 
+nvcc --version
+nvidia-smi
+
 set +x
