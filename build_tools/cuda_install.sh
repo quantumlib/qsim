bf7d0297fd8e3fa5ac61f7419c1f1af7af0697d2
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2021-12-10 08:41:54 -0800

    move script into qsim

--- build_tools/cuda_install.sh
+++ build_tools/cuda_install.sh
@@ -1,4 +1,25 @@
-curl https://raw.githubusercontent.com/GoogleCloudPlatform/compute-gpu-installation/main/linux/install_gpu_driver.py --output install_gpu_driver.py
+#!/bin/bash
+set -x
 
-# run the installer script
-python3 install_gpu_driver.py --force
+# install dependencies
+kernel_version=$(uname -r)
+yum clean all
+yum install -y kernel
+yum install -y kernel-devel-${kernel_version} kernel-headers-${kernel_version} pciutils
+
+# install CUDA toolkit
+
+yum install -y yum-utils epel-release
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+yum clean all
+yum -y install nvidia-driver-latest-dkms cuda
+yum -y install cuda-drivers
+
+# post-install actions
+
+CUDA_PROFILE="/etc/profile.d/cuda.sh"
+echo "# CUDA Toolkit settings" >> $CUDA_PROFILE
+echo "export PATH=/usr/local/cuda-11/bin:$PATH" >> $CUDA_PROFILE
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-11/lib64:$LD_LIBRARY_PATH" >> $CUDA_PROFILE
+
+set +x
