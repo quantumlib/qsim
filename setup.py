b7c2137e4cbe78d5c09b54e82d489603376639c1
Orion Martin <40585662+95-martin-orion@users.noreply.github.com>
2022-07-13 09:19:48 -0700

    skip musllinux, add DMS_WIN64

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ class CMakeBuild(build_ext):
             ]
             if sys.maxsize > 2**32:
                 cmake_args += ["-A", "x64"]
+            env["CXXFLAGS"] += " -DMS_WIN64"
             build_args += ["--", "/m"]
         else:
             cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
@@ -64,9 +65,7 @@ class CMakeBuild(build_ext):
             ]
 
         env = os.environ.copy()
-        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get("CXXFLAGS", ""), self.distribution.get_version()
-        )
+        env["CXXFLAGS"] += f' -DVERSION_INFO=\\"{self.distribution.get_version()}\\"'
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(
