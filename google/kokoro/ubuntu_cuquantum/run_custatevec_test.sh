2a719ef57d5ee6c7db3109a1a8b883dc741cb6c1
Jae Yoo <jaeyoo@google.com>
2023-04-11 23:22:03 -0700

    Fix working dir

--- google/kokoro/ubuntu_cuquantum/run_custatevec_test.sh
+++ google/kokoro/ubuntu_cuquantum/run_custatevec_test.sh
@@ -8,9 +8,8 @@ python3 -m venv ./env
 source env/bin/activate
 
 # Run cuQuantum tests.
-echo ${PWD}
-ls -al
-export CUQUANTUM_DIR=${PWD}/cuquantum
+export CUQUANTUM_DIR=/workspace/cuquantum
 export CUQUANTUM_ROOT=${CUQUANTUM_DIR}
 export LD_LIBRARY_PATH=${CUQUANTUM_ROOT}/lib:${LD_LIBRARY_PATH}
+cd workspace
 make run-custatevec-tests
