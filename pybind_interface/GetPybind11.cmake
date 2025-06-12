30fcf02c9f4c9942bc979b6dca9fdc7b30145cae
mhucka <mhucka@google.com>
2025-06-12 17:59:28 +0000

    Rewrite GetPybind11.cmake to be more robust
    On some platforms, the previous pybind-finding code either found the
    wrong one, or didn't set certain cmake variables.

--- pybind_interface/GetPybind11.cmake
+++ pybind_interface/GetPybind11.cmake
@@ -1,28 +1,40 @@
-include(FetchContent)
-
 set(MIN_PYBIND_VERSION "2.13.6")
-FetchContent_Declare(
-  pybind11
-  GIT_REPOSITORY https://github.com/pybind/pybind11
-  GIT_TAG "v${MIN_PYBIND_VERSION}"
-)
-FetchContent_GetProperties(pybind11)
-find_package(pybind11 "${MIN_PYBIND_VERSION}" CONFIG)
 
-if (pybind11_FOUND)
-  message(STATUS "Found pybind11 v${pybind11_VERSION}: ${pybind11_INCLUDE_DIRS}")
-  # The pybind11_add_module doesn't correctly set the CXX_INCLUDES properly if a system pybind11 is found.
-  # Using `include_directories(${pybind11_INCLUDE_DIRS})` doesn't result in anything in
-  # CXX_INCLUDES. e.g., `pybind_interface/basic/CMakeFiles/qsim_basic.dir/flags.make` would only
-  # have `CXX_INCLUDES = -isystem $PREFIX/include/python3.11` and would miss `$PREFIX/include`.
-  # This problem would result in `fatal error: pybind11/complex.h: No such file or directory`
-  # This is a hack to get around that by passing `-I/path/to/include` to CXX_FLAGS
-  # Iterate over each include directory and add it as a compile option
-  foreach(INCLUDE_DIR ${pybind11_INCLUDE_DIRS})
-    add_compile_options("-I${INCLUDE_DIR}")
-  endforeach()
+# First, try to find pybind11 using the modern find_package() way.
+find_package(pybind11 ${MIN_PYBIND_VERSION} QUIET)
+
+# If that doesn't work, try to use pybind11-config to find the CMake directory.
+if(NOT pybind11_FOUND)
+  find_program(PYBIND11_CONFIG_EXECUTABLE pybind11-config)
+
+  if(PYBIND11_CONFIG_EXECUTABLE)
+    # Get the CMake directory from pybind11-config
+    execute_process(
+      COMMAND ${PYBIND11_CONFIG_EXECUTABLE} --cmakedir
+      OUTPUT_VARIABLE pybind11_CMAKE_DIR
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    # Add the discovered path to the CMake search paths and try again.
+    if(pybind11_CMAKE_DIR AND IS_DIRECTORY ${pybind11_CMAKE_DIR})
+      list(APPEND CMAKE_PREFIX_PATH ${pybind11_CMAKE_DIR})
+      find_package(pybind11 ${MIN_PYBIND_VERSION} QUIET)
+    endif()
+  endif()
 endif()
 
-if((NOT pybind11_FOUND) AND (NOT pybind11_POPULATED)) # check first on system path, then attempt git fetch
+# If pybind11 is still not found, use FetchContent to download it.
+if(NOT pybind11_FOUND)
+  message(STATUS "pybind11 not found. Fetching from source.")
+  include(FetchContent)
+
+  FetchContent_Declare(
+    pybind11
+    GIT_REPOSITORY https://github.com/pybind/pybind11
+    GIT_TAG "v${MIN_PYBIND_VERSION}"
+    OVERRIDE_FIND_PACKAGE
+  )
   FetchContent_MakeAvailable(pybind11)
+else()
+  message(STATUS "Found pybind11: ${pybind11_VERSION} (found version: ${pybind11_VERSION})")
 endif()
