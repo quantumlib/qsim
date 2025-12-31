30f0da9eac0552aae09e270d613c37414b1cb645
mhucka <mhucka@google.com>
2025-12-31 22:37:55 +0000

    Run through Black formatter

--- setup.py
+++ setup.py
@@ -111,7 +111,8 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(
-            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp,
+            ["cmake", ext.sourcedir] + cmake_args,
+            cwd=self.build_temp,
         )
         subprocess.check_call(
             ["cmake", "--build", ".", "--verbose"] + build_args,
