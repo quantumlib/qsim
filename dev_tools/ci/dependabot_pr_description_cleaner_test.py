6e8574ee0a5e019389f55d945dc7b098a1ef8a78
mhucka <mhucka@google.com>
2026-01-02 20:08:53 +0000

    Disable pylint long-line warnings for this test file

--- dev_tools/ci/dependabot_pr_description_cleaner_test.py
+++ dev_tools/ci/dependabot_pr_description_cleaner_test.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=line-too-long
+
 import dependabot_pr_description_cleaner as cleaner
 import pytest
 
