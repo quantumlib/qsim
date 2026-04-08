dc48a21fb017e02551a62ecb85ae1d33c6b199f5
Sergei Isakov <54642992+sergeisakov@users.noreply.github.com>
2026-04-08 17:24:02 +0200

    Address lint checks.

--- qsimcirq/qsim_circuit.py
+++ qsimcirq/qsim_circuit.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List, Sequence, Tuple, Union
+from typing import Dict, List, Sequence, Tuple
 
 import cirq
 import numpy as np
